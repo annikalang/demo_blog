bc9b9cd474e63079d191d7c27c8deee87775cbde
Annika Lang <Lang-annika@t-online.de>
2019-10-08 10:26:39 +0200

    added post params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,20 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params) # see below
+  end
+
+  private
+
+  # for security reasons, to prevent that only permit the title and content from
+  # being input to the form when creating a post, otherwhise you could submit any data to the server
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
